0a8e24468033080661730d648f7340ca1358d1a9
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-04-19 16:27:45 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,20 +35,23 @@ const Home: NextPage = () => (
       <div className="max-w-prose m-auto flex flex-col gap-3">
         <h3 id="start">Hello!</h3>
         <p>
-          I&apos;m Elijah, an undergraduate student at UMass Amherst double-majoring in computer science and statistics/data science.
-          Most of my professional experience is in frontend web development, and in my personal projects I've
-          done work on the full stack from databases to web servers and cloud hosting.
+          I&apos;m Elijah, an undergraduate student at UMass Amherst double-majoring in
+          computer science and statistics/data science. Most of my professional experience
+          is in frontend web development, and in my personal projects I&apos;ve done work
+          on the full stack from databases to web servers and cloud hosting.
         </p>
         <p>
           My last internship was at RelationalAI where I created a data-informed, interactive
           diagram component for a user training website written with TS, Next.js, and TailwindCSS.
         </p>
         <p>
-          In addition to CS, I&apos;m passionate about using technology for things that matter to me personally—as a learner and teacher at
+          In addition to CS, I&apos;m passionate about using technology for things that matter
+          to me personally—as a learner and teacher at
           heart, these projects often have to do with education. Whether it&apos;s making
           <a href="https://elijahcalc.netlify.app/euler/" target="_blank" rel="noopener noreferrer"> calculus visualizations</a>
-          or <a href="https://beautifuldatascience.netlify.app/" target="_blank" rel="noopener noreferrer">visualizing machine learning concepts</a>,
-          I have a lot of fun creating tools that help students learn more effectively.
+          or
+          <a href="https://beautifuldatascience.netlify.app/" target="_blank" rel="noopener noreferrer">visualizing machine learning concepts</a>
+          , I have a lot of fun creating tools that help students learn more effectively.
         </p>
         <p>
           Lots more coming to this website soon.
