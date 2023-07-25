5c4beb853140a5c4a0ca01c25d822c2b2b5edaf8
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-07-25 14:53:13 -0400

    try to optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import mdx from '@astrojs/mdx';
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +18,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    mdx()
   ],
   markdown: {
     remarkPlugins: [
